2ed08f631a318903c54d33565487052d5fb88773
Lucas Waldburger <lucas.c.waldburger@gmail.com>
2023-03-07 09:36:56 -0500

    add guess counter add temporary display to visualize ship locations

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 
 
 
+//////////////////////////SHIP LOGIC//////////////////////////
+
+// CONSTS //
+const int gridLength = 7;
+const std::string border = "  --1------2------3------4------5------6------7--";
+
 
 // STRUCTS //
-
 struct Segment {
     int rowValue;
     int columnValue;
@@ -16,25 +21,14 @@ struct Ship {
     int NumberOfSegments;
     bool isHorizontal;
     std::vector<Segment> segmentLocations;
-
-//    bool operator==(const Ship& otherShip) const {
-//        if (NumberOfSegments == otherShip.NumberOfSegments) {
-//            return true;
-//        }
-//        return false;
-//    }
 };
 
-
-
 // OPERATOR OVERLOADS //
-
 std::ostream& operator<<(std::ostream& stream, const Ship& ship) {
     for (Segment seg: ship.segmentLocations)
         stream << "(row: " << seg.rowValue << ", column: " << seg.columnValue << "), ";
     return stream;
 }
-
 
 bool operator==(const Segment& shipSegment, const Segment& playerGuess) {
     if (playerGuess.rowValue == shipSegment.rowValue &&
@@ -44,13 +38,13 @@ bool operator==(const Segment& shipSegment, const Segment& playerGuess) {
     return false;
 }
 
-
 // FUNCTIONS //
 
-//EnemyPieces& createShips(int shipSizes[5]) {
-//
-//}
+int getRandomNum(int upperBound) {
+    return arc4random() % upperBound;
+}
 
+//initial ship placement
 void plotSegments(Ship& ship, int startRow, int startColumn) {
     for (int i = 0; i < ship.NumberOfSegments; i++) {
         if (ship.isHorizontal)
@@ -62,6 +56,91 @@ void plotSegments(Ship& ship, int startRow, int startColumn) {
                     Segment{startRow + i, startColumn}
             );
     }
+}
+
+int modifyEmptySpaces(int row, int column, Segment& seg, int adjacentEmptySpaces) {
+    if (row == seg.rowValue && column == seg.columnValue)
+        adjacentEmptySpaces = 0;
+    else
+        adjacentEmptySpaces += 1;
+    return adjacentEmptySpaces;
+}
+
+void findLegalPlacement(Ship& ship) {
+    int adjacentEmptySpaces = 0;
+    int rowStart = 0;
+    int columnStart = 0;
+    int randomNum = getRandomNum(gridLength - (ship.NumberOfSegments - 1));
+
+    if (ship.isHorizontal)
+        rowStart = randomNum;
+    else
+        columnStart = randomNum;
+
+    std::cout << "Starting Row: " << rowStart << '\n'
+              << "Starting Column: " << columnStart << '\n'
+              << "Number of segment locations: " << ship.segmentLocations.size() << std::endl;
+
+    while (ship.segmentLocations.size() < ship.NumberOfSegments) {
+        for (int row = rowStart; row < gridLength; row++) {
+            for (int col = columnStart; col < gridLength; col++) {
+                for (Segment seg: ship.segmentLocations) {
+                    if (row == seg.rowValue && col == seg.columnValue) {
+                        std::cout << "re-do!!";
+                        findLegalPlacement(ship);
+                    }
+                }
+                adjacentEmptySpaces += 1;
+                std::cout << "Adjacent Empty Spaces Counter: " << adjacentEmptySpaces << std::endl;
+
+                if (adjacentEmptySpaces >= ship.NumberOfSegments) {
+                    if (ship.isHorizontal) {
+                        plotSegments(ship, row, col - (ship.NumberOfSegments - 1));
+                    }
+                    else {
+                        plotSegments(ship, col - (ship.NumberOfSegments - 1), col);
+                    }
+                    std::cout << ship << '\n' << std::endl;
+                    return;
+                }
+            }
+        }
+    }
+//    else {
+//        columnStart = randomNum;
+//
+//        std::cout << "Starting Column: " << columnStart << '\n'
+//                  << "Number of segment locations: " << ship.segmentLocations.size() << std::endl;
+//
+//        while (ship.segmentLocations.size() < ship.NumberOfSegments) {
+//            for (int col = columnStart; col < gridLength; col++) {
+//                for (int row = rowStart; row < gridLength; row++) {
+//                    for (Segment seg: ship.segmentLocations) {
+//                        if (row == seg.rowValue && col == seg.columnValue) {
+//                            adjacentEmptySpaces = 0;
+//                            break;
+//                        }
+//                    }
+//                    adjacentEmptySpaces += 1;
+//                    std::cout << "Adjacent Empty Spaces Counter: " << adjacentEmptySpaces << std::endl;
+//                    if (adjacentEmptySpaces >= ship.NumberOfSegments) {
+//                        plotSegments(ship, row - (ship.NumberOfSegments - 1), col);
+//                        std::cout << ship << '\n' << std::endl;
+//                        return;
+//                    }
+//                }
+//            }
+//        }
+//    }
+}
+
+
+
+//player guess
+
+
+void increaseGuessCounter(int& guessCounter) {
+    guessCounter += 1;
 }
 
 int convertAlpha(char alpha) {
@@ -77,22 +156,8 @@ Segment getPlayerGuess() {
     std::cout << "Enter your guess: ";
     std::cin >> rowGuess >> columnGuess;
 
-    Segment guessAsSegment = {convertAlpha(rowGuess), columnGuess};
+    Segment guessAsSegment = {convertAlpha(rowGuess), columnGuess - 1};
     return guessAsSegment;
-}
-
-
-
-Ship * getHitShip(std::vector<Ship>& allPieces, Segment& playerGuess) {
-    for (Ship& ship: allPieces) {
-        for (Segment& seg: ship.segmentLocations) {
-            if (playerGuess == seg) {
-                std::cout << "Address of ship found within getHitShip" << &ship;
-                return &ship;
-            }
-        }
-    }
-    return nullptr;
 }
 
 int getHitShipIndex(std::vector<Ship>& allPieces, Segment& playerGuess) {
@@ -106,17 +171,6 @@ int getHitShipIndex(std::vector<Ship>& allPieces, Segment& playerGuess) {
     return -1;
 }
 
-bool isSegmentSunk(std::vector<Ship>& allPieces) {
-    for (const Ship& ship: allPieces) {
-        for (Segment seg: ship.segmentLocations) {
-            if (seg.isSunk) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 // run through each segment of one ship
 bool isWholeShipSunk(Ship& ship) {
     if (ship.segmentLocations.empty()) {
@@ -124,7 +178,6 @@ bool isWholeShipSunk(Ship& ship) {
     }
     return false;
 }
-
 
 // run through each ship of all enemy pieces to see if won
 bool areAllShipsSunk(std::vector<Ship>& allPieces) {
@@ -134,12 +187,10 @@ bool areAllShipsSunk(std::vector<Ship>& allPieces) {
     return false;
 }
 
-
 void removeSegmentFromShip(Ship& ship, Segment& hitSegment) {
     std::vector<Segment>::iterator it;
     it = std::remove(ship.segmentLocations.begin(), ship.segmentLocations.end(), hitSegment);
     ship.segmentLocations.erase(it, ship.segmentLocations.end());
-    std::cout << "inside remove segment function: " << &ship << std::endl;
 }
 
 void removeShipFromPieces(std::vector<Ship>& allPieces) {
@@ -148,33 +199,120 @@ void removeShipFromPieces(std::vector<Ship>& allPieces) {
     allPieces.erase(it, allPieces.end());
 }
 
+////////////////////////// VISUAL GRID //////////////////////////
+//Printing
+void clearGridVisual(char grid[gridLength][gridLength]) {
+    for (int row = 0; row < gridLength; row++)
+        for (int col = 0; col < gridLength; col++)
+            grid[row][col] = ' ';
+}
+
+//void printGridVisual(char grid[gridLength][gridLength], int& guessCounter) {
+//    std::cout << border << std::endl;
+//    for (int row = 0; row < gridLength; row++) {
+//        for (int col = 0; col < gridLength; col++) {
+//            if (col == 0) {
+//                std::cout << static_cast<char>(row + 65) << ' ';
+//            }
+//            std::cout << "[ " << grid[row][col] << " ]  ";
+//            if (col == 6) {
+//                std::cout << '\n';
+//                if (row != 6)
+//                    std::cout << '\n';
+//            }
+//        }
+//    }
+//    std::cout << border << '\n'
+//              << "\t\t\t\t" << "Guess Counter: " << guessCounter << '\n' << std::endl;
+//}
+
+void printGridVisual(char grid[gridLength][gridLength], std::vector<Ship>& allPieces, int& guessCounter) {
+    std::cout << border << std::endl;
+    for (int row = 0; row < gridLength; row++) {
+        for (int col = 0; col < gridLength; col++) {
+            if (col == 0) {
+                std::cout << static_cast<char>(row + 65) << ' ';
+            }
+            for (Ship ship: allPieces) {
+                for (Segment seg: ship.segmentLocations) {
+                    if (row == seg.rowValue && col == seg.columnValue) {
+                        grid[row][col] = 'O';
+                        break;
+                    }
+                }
+            }
+            std::cout << "[ " << grid[row][col] << " ]  ";
+            if (col == 6) {
+                std::cout << '\n';
+                if (row != 6)
+                    std::cout << '\n';
+            }
+        }
+    }
+    std::cout << border << '\n'
+              << "\t\t\t\t" << "Guess Counter: " << guessCounter << '\n' << std::endl;
+}
+
+void drawHitResult(char grid[gridLength][gridLength], Segment& playerGuess, char result) {
+    for (int row = 0; row < gridLength; row++) {
+        for (int col = 0; col < gridLength; col++) {
+            std::cout << '(' << row << ", " << col << ')' << '\n' << std::endl;
+            if (row == playerGuess.rowValue && col == playerGuess.columnValue &&
+                grid[row][col] == ' ') {
+                grid[row][col] = result;
+                break;
+            }
+        }
+    }
+}
+
 
 int main() {
 
     std::vector<Ship>allEnemyPieces;
+    int guessCounter = 0;
 
     // PUT ALL SHIPS IN IMPLEMENTATION FILE //
-    allEnemyPieces.push_back({5, true, false,{
-            {0, 2, false},
-            {0, 3, false},
-            {0, 4, false},
-            {0, 5, false},
-            {0, 6, false}}});
+    allEnemyPieces.push_back({5, true});
+    allEnemyPieces.push_back({4, false});
+    allEnemyPieces.push_back({3, true});
+    allEnemyPieces.push_back({3, false});
+    allEnemyPieces.push_back({2, true});
 
+    for (auto & ship : allEnemyPieces) {
+        findLegalPlacement(ship);
+    }
+
+    char visualGrid[7][7];
+    clearGridVisual(visualGrid);
 
     while (!allEnemyPieces.empty()) {
+
+        printGridVisual(visualGrid, allEnemyPieces, guessCounter);
+
         Segment playerGuess = getPlayerGuess();
+
+        increaseGuessCounter(guessCounter);
 
         std::cout << "Player Guess: (" << playerGuess.rowValue << ", " << playerGuess.columnValue << ')' << std::endl;
 
         int hitShipIndex = getHitShipIndex(allEnemyPieces, playerGuess);
 
+        std::cout << "Hit Ship Index Position: " << hitShipIndex << '\n' << std::endl;
+
         if (hitShipIndex != -1) {
             removeSegmentFromShip(allEnemyPieces[hitShipIndex], playerGuess);
+            removeShipFromPieces(allEnemyPieces);
+
+            drawHitResult(visualGrid, playerGuess, 'O');
+        }
+        else {
+            drawHitResult(visualGrid, playerGuess, 'X');
         }
 
+
         for (Ship &ship: allEnemyPieces) {
-            std::cout << ship;
+            std::cout << ship << std::endl;
         }
     }
 
